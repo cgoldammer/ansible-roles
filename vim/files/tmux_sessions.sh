f1cc7f24fd20fe61b524810d39c0ae29dfb9782b
cgoldammer <goldammer.christian@gmail.com>
2016-05-12 08:20:20 -0400

    Added role for vim and tmux setup

---
+++ vim/files/tmux_sessions.sh
@@ -0,0 +1,8 @@
+# avoid duplicates..
+export HISTCONTROL=ignoredups:erasedups
+
+# append history entries..
+shopt -s histappend
+
+# After each command, save and reload history
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
